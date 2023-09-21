3f823bd2afbcb61555097f71b30dd78b5943fa78
lior.yardeni <lior.yardeni@lseg.com>
2023-09-21 17:32:46 +0300

    minimal gsst, initial complaxed azure sst

---
+++ src/text_to_speech_azure.py
@@ -0,0 +1,4 @@
+import azure.cognitiveservices.speech as speechsdk
+
+# requires to create speech source, in a specific speech region
+# https://learn.microsoft.com/en-us/azure/ai-services/speech-service/get-started-text-to-speech?tabs=macos%2Cterminal&pivots=programming-language-python
