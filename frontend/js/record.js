5d5a90c4cf912ce23043684b5752c4d1f0ac26d8
evoosa <evaosher@gmail.com>
2023-09-21 14:09:11 +0300

    changed

--- frontend/js/record.js
+++ frontend/js/record.js
@@ -3,38 +3,60 @@ const stopRecordButton = document.getElementById("stopRecord");
 const audioPlayer = document.getElementById("audioPlayer");
 
 let mediaRecorder;
-let audioChunks = [];
+// let audioChunks = [];
+let isRecording = false;
 
-startRecordButton.addEventListener("click", startRecording);
-stopRecordButton.addEventListener("click", stopRecording);
+function startRecording() {
+    navigator.mediaDevices.getUserMedia({audio: true})
+        .then(function (stream) {
+            mediaRecorder = new MediaRecorder(stream);
+            let audioChunks = [];
 
-async function startRecording() {
-    const stream = await navigator.mediaDevices.getUserMedia({audio: true});
+            mediaRecorder.ondataavailable = (event) => {
+                if (event.data.size > 0) {
+                    audioChunks.push(event.data);
+                }
+            };
 
-    mediaRecorder = new MediaRecorder(stream);
+            mediaRecorder.onstop = () => {
+                const audioBlob = new Blob(audioChunks, {type: "audio/wav"});
+                const audioUrl = URL.createObjectURL(audioBlob);
+                audioPlayer.src = audioUrl;
+            };
 
-    mediaRecorder.ondataavailable = (event) => {
-        if (event.data.size > 0) {
-            audioChunks.push(event.data);
-        }
-    };
-
-    mediaRecorder.onstop = () => {
-        // const audioBlob = new Blob(audioChunks, {type: "audio/ogg; codecs=opus"});
-        const audioBlob = new Blob(audioChunks, {type: "audio/wav"});
-        const audioUrl = URL.createObjectURL(audioBlob);
-        audioPlayer.src = audioUrl;
-    };
-
-    mediaRecorder.start();
-    startRecordButton.disabled = true;
-    stopRecordButton.disabled = false;
+            mediaRecorder.start();
+            isRecording = true;
+            startRecordButton.disabled = true;
+            stopRecordButton.disabled = false;
+        })
+        .catch(function (error) {
+            console.error("Error accessing microphone:", error);
+        });
 }
 
 function stopRecording() {
-    if (mediaRecorder && mediaRecorder.state === "recording") {
+    if (mediaRecorder && isRecording) {
         mediaRecorder.stop();
+        isRecording = false;
         startRecordButton.disabled = false;
         stopRecordButton.disabled = true;
     }
 }
+
+document.addEventListener("keydown", function (event) {
+    if (event.key == " " || event.code == "Space") {
+        if (!isRecording) {
+            startRecording();
+            console.log("START RECORDING");
+        }
+    }
+});
+
+document.addEventListener("keyup", function (event) {
+    if (event.key == " " || event.code == "Space") {
+        if (isRecording) {
+            stopRecording();
+            console.log("STOP RECORDING");
+        }
+    }
+});
