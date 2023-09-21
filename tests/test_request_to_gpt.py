3a275bff280356838616c6623e1326739770c7be
NaomiKriger <naomikriger@gmail.com>
2023-09-21 21:42:05 +0300

    refactor system instructions construction

--- tests/test_request_to_gpt.py
+++ tests/test_request_to_gpt.py
@@ -2,7 +2,7 @@ import unittest
 
 from dotenv import load_dotenv
 
-from src.commons import constant_instruction
+from src.commons import get_system_instructions
 from src.text_to_ai_integration import make_openai_request
 
 load_dotenv()
@@ -10,11 +10,8 @@ load_dotenv()
 
 # NOTE - MAKING A REAL CALL TO GPT, NEED TO PATCH THIS
 class TestGptRequest(unittest.TestCase):
-    chosen_figure_by_user = "drunk future teller"
-    request_based_instruction = f" You are: {chosen_figure_by_user}"
-    user_question = "What does Harry Potter like in Hogwarts?"
 
     def test_gpt_request(self):
-        res = make_openai_request(system_instruction=constant_instruction + self.request_based_instruction,
-                                  user_question=self.user_question)
+        res = make_openai_request(system_instruction=get_system_instructions(figure="drunk future teller"),
+                                  user_question="What does Harry Potter like in Hogwarts?")
         assert res is not None
