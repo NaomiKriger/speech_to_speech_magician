33a3985ef49741ede9a4f46f56710331cb17292e
NaomiKriger <naomikriger@gmail.com>
2023-09-22 15:16:43 +0300

    enable "new" flow

--- main.py
+++ main.py
@@ -20,8 +20,10 @@ def choose_figure():
     while True:
         choice = input("\nEnter the number of your chosen figure: ")
         try:
-            if choice.lower() == "exit" or choice.lower() == "new":
-                return choice
+            if choice.lower() == "exit":
+                return "exit"
+            elif choice.lower() == "new":
+                return "new"
 
             choice = int(choice)
             if 1 <= choice <= len(figure_options):
@@ -52,21 +54,20 @@ def play_gpt_response(transcript: str):
     return "playing response"
 
 
-def is_another_round() -> bool:
+def is_another_round() -> str:
     choice = input("Do you want to play another round? \n"
-                   "Type 'yes' or 'no'")
+                   f"Type 'yes' or 'no'. {exit_option}")
+
     while True:
         try:
-            if choice.lower() == 'yes':
-                return True
-            elif choice.lower() == 'no':
-                return False
+            if choice.lower() in ["exit", "new", "yes", "no"]:
+                return choice.lower()
             else:
                 print("No valid option was chosen. Please try again")
                 choice = input("Do you want to play another round? \n"
                                "Type 'yes' or 'no'")
         except Exception as e:
-            pass
+            print(e)
 
 
 def finish():
@@ -78,8 +79,11 @@ def main():
     start()
     user_choice = choose_figure()
 
-    if user_choice.lower() == "exit":
+    if user_choice == "exit":
         print("Finishing. Was great having you here, hope to see you again soon!")
+
+    elif user_choice == "new":
+        main()
 
     else:
         another_round = True
@@ -88,7 +92,12 @@ def main():
             transcription = get_transcription(audio_input=audio_stream)
             gpt_answer = get_gpt_answer(transcription=transcription, figure=user_choice)
             play_gpt_response(gpt_answer)
-            another_round = is_another_round()
+            user_choice = is_another_round()
+            if user_choice == "new":
+                main()
+            elif user_choice in ["exit", "no"]:
+                print("Finishing. Was great having you here, hope to see you again soon!")
+                another_round = False
 
 
 if __name__ == "__main__":
